d2c0e840c37757c4674b4411cb9fad9d26837128
Abdusamid <muhammadovabdusamid07@gmail.com>
2022-07-04 13:24:10 +0500

    GridView LinearLayout

--- app/src/main/java/developer/abdusamid/homework31/MainActivity.kt
+++ app/src/main/java/developer/abdusamid/homework31/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        supportActionBar?.hide()
         loadData()
 
         userAdapter = UserAdapter(list)
